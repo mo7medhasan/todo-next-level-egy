1afe81a8156ad33249d3b97b4a30868deeb4f77f
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2024-01-05 15:26:18 +0200

    change style popup

--- src/components/AuthComponents.js
+++ src/components/AuthComponents.js
@@ -12,7 +12,7 @@ const NavigateHome = ({showPopup}) => {
             : "popup hidden"
         }
       >
-        <section className="popup-main  shadow p-5  bg-white rounded-lg  max-w-xs w-full h-[50%]">
+        <section className="popup-main  shadow p-5 flex justify-center items-center bg-white rounded-lg  max-w-xs w-full h-[50%]">
           <NavLink
             to="/"
             className={
